c83c2e2f7eef352b4e5ebd103cbad6f018391057
andrew-wilkes <github@urgentclick.com>
2022-11-22 19:38:43 +0900

    Use a bool for direction flag

--- Main.gd
+++ Main.gd
@@ -1,29 +1,36 @@
 extends Node
 
 var anim_target_position = 0
-var anim_position = 0
+var anim_current_position = 0
+var anim_playing_forward = true
 
 func _on_HSlider_value_changed(value):
 	set_target_position(value / 100)
 
 
-func set_target_position(pos):
+func set_target_position(pos): # 0.0 .. 1.0
 	anim_target_position = pos
 
 
 func _process(_delta):
 	if $Anim.is_playing():
-		anim_position = $Anim.current_animation_position / $Anim.current_animation_length
+		anim_current_position = $Anim.current_animation_position / $Anim.current_animation_length
 	# Check for closeness to target position
-	if abs(anim_target_position - anim_position) < 0.01:
+	if abs(anim_target_position - anim_current_position) < 0.01:
 		if $Anim.is_playing():
 			$Anim.stop(false)
 	else:
-		if anim_target_position > anim_position:
-			if not $Anim.is_playing() or $Anim.playback_speed < 0:
-				# Play forward to advance towards the target position
+		if anim_target_position > anim_current_position:
+			# Play forward to advance towards the target position
+			if not $Anim.is_playing():
 				$Anim.play("Animate")
+			elif not anim_playing_forward:
+				$Anim.play("Animate")
+			anim_playing_forward = true
 		else:
-			if not $Anim.is_playing() or $Anim.playback_speed > 0:
-				# Play backwards to move back towards the target position
+			# Play backwards to move back towards the target position
+			if not $Anim.is_playing():
 				$Anim.play_backwards("Animate")
+			elif anim_playing_forward:
+				$Anim.play_backwards("Animate")
+			anim_playing_forward = false
